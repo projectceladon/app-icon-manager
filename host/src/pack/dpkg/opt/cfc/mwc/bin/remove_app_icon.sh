bec25c3384cc2cf3fdd38826ff85e661bb48132e
Wan Shuang <shuang.wan@intel.com>
2021-09-24 15:12:15 +0800

    Added the app icon removal script
    Added the app icon removal script;
    Added the script for user to create app luanch icon in desktop
    directly;
    Updated the echo script to make the output more clear.
    
    Signed-off-by: Wan Shuang <shuang.wan@intel.com>
    Tracked-On: OAM-99597

---
+++ host/src/pack/dpkg/opt/cfc/mwc/bin/remove_app_icon.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sfilename=$0
+
+if [ "x${sfilename}" == "x-bash" ]; then
+	sfilename=$BASE_SOURCE
+fi
+
+exec_pwd=$(cd $(dirname $sfilename);pwd)
+
+echo "Start to remove app .desktop file for Kylin OS ..."
+
+dir_desktop_app=${HOME}/applications
+
+cd $dir_desktop_app
+NUM_PARAMS=$#
+
+if [[ "$NUM_PARAMS" -eq "1" ]];
+then
+  echo "Remove kylin launch icon for given app..."
+  app_pkg=$1
+  lunch_file="$1.desktop"
+  if [[ -f $lunch_file ]];
+  then
+    echo "Processing app lunch file for kylin: $lunch_file"
+    app_launch_file="/usr/share/applications/$lunch_file"
+    if [[ -f $app_lunch_file ]];
+    then
+        sudo rm $app_lunch_file
+    fi
+
+    cd -
+    echo "Start to update desktop app list for Kylin OS ..."
+    desktop_applist_file=${HOME}/.config/ukui/desktop_applist
+    desktop_menu_file=${HOME}/.config/ukui/ukui-menu.ini
+    sed -i "\%$lunch_file%d" $desktop_applist_file
+    sed -i "\%$lunch_file%d" $desktop_menu_file
+  fi
+else
+  echo "Please specify the app package name to be removed!"
+fi
+
+echo "Kill the process ukui-menu for it restart with updated app list..."
+
+ukui_menu_pid=`ps aux | grep ukui-menu | grep -v grep | awk '{print $2}'`
+kill -KILL $ukui_menu_pid
+
+echo "Android app lunch icon removal done."
---
+++ host/src/pack/dpkg/opt/cfc/mwc/bin/update_lunch_pg_cmdline.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+sfilename=$0
+
+if [ "x${sfilename}" == "x-bash" ]; then
+	sfilename=$BASE_SOURCE
+fi
+
+exec_pwd=$(cd $(dirname $sfilename);pwd)
+
+echo "Start to update app .desktop file for Kylin OS ..."
+
+#dir_desktop_app=${HOME}/.local/share/applications/
+dir_desktop_app=${HOME}/applications
+
+cd $dir_desktop_app
+NUM_PARAMS=$#
+
+if [[ "$NUM_PARAMS" -eq "1" ]];
+then
+  echo "Update kylin launch icon for given app..."
+  app_pkg=$1
+  lunch_file="$1.desktop"
+  if [[ -f $lunch_file ]];
+  then
+    echo "Processing app lunch file for kylin: $lunch_file"
+    sed -i "s%^Exec=\/%Exec=sh -- \/%g" $lunch_file
+    echo "Copy app .desktop file: $lunch_file to /usr/share/applications/ folder."
+    sudo cp "$lunch_file" /usr/share/applications/
+    cd -
+    echo "Start to update desktop app list for Kylin OS ..."
+    desktop_applist_file=${HOME}/.config/ukui/desktop_applist
+    desktop_menu_file=${HOME}/.config/ukui/ukui-menu.ini
+    cat $desktop_applist_file | grep -q $lunch_file
+    if [ "1" == $? ]
+    then
+      echo "Append the app lunch icon to kylin app luancher"
+      echo $lunch_file >>$desktop_applist_file
+    fi
+    # Add the .desktop file into ukui-menu.ini
+    $exec_pwd/update_pg_menu.py $lunch_file $desktop_menu_file
+  fi
+else
+  echo "Update kylin luanch icons in batch mode..."
+  lunch_files="`ls *.desktop`"
+  for app_desktop_file in $lunch_files
+  do
+     echo "Processing app lunch file for kylin: $app_desktop_file"
+     sed -i "s%^Exec=\/%Exec=sh -- \/%g" $app_desktop_file
+
+  done
+  echo "Copy app .desktop files to /usr/share/applications/ folder..."
+  sudo cp *.desktop /usr/share/applications/
+  cd -
+
+  echo "Start to update desktop app list for Kylin OS ..."
+  desktop_applist_file=${HOME}/.config/ukui/desktop_applist
+  desktop_menu_file=${HOME}/.config/ukui/ukui-menu.ini
+
+  for app_desktop_file in $lunch_files
+  do
+     cat $desktop_applist_file | grep -q $app_desktop_file
+     if [ "1" == $? ]
+     then
+	   echo "Append the app lunch icon to kylin app luancher"
+	   echo $app_desktop_file >>$desktop_applist_file
+     fi
+     # Add the .desktop file into ukui-menu.ini
+     $exec_pwd/update_pg_menu.py $app_desktop_file $desktop_menu_file
+  done
+fi
+
+echo "Kill the process ukui-menu for it restart with updated app list..."
+
+ukui_menu_pid=`ps aux | grep ukui-menu | grep -v grep | awk '{print $2}'`
+kill -KILL $ukui_menu_pid
+
+echo "Android app lunch icon updating done."
