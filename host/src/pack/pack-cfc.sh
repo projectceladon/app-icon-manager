97cc4c45c139ab604561d3f78ebd74b1d33c3192
Yadong Qi <yadong.qi@intel.com>
2021-11-12 15:06:27 +0800

    Remove duplicated script: lg_launcher.sh
    Signed-off-by: Yadong Qi <yadong.qi@intel.com>

--- host/src/pack/pack-cfc.sh
+++ host/src/pack/pack-cfc.sh
@@ -3,7 +3,6 @@ cp ../pghost/mwc_hostdaemon dpkg/opt/cfc/mwc/bin/mwc_hostdaemon
 cp ../pghost/mwc_launcher dpkg/opt/cfc/mwc/bin/mwc_launcher
 cp ../pghost/loadapp.sh dpkg/opt/cfc/mwc/bin/loadapp.sh
 cp ../pghost/killapp.sh dpkg/opt/cfc/mwc/bin/killapp.sh
-cp ../pghost/lg_launcher.sh dpkg/opt/cfc/mwc/bin/lg_launcher.sh
 cp ../pghost/closeapp.sh dpkg/opt/cfc/mwc/bin/closeapp.sh
 cp ../pghost/api_test dpkg/opt/cfc/mwc/bin/api_test
 cp ../pghost/libkydroid.so dpkg/opt/cfc/mwc/lib/libkydroid.so
