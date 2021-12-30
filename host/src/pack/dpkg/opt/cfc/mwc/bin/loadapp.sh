b427c829a49d2e6ee68f092814848d11159a5b17
Wan Shuang <shuang.wan@intel.com>
2021-12-30 13:37:08 +0800

    Porting AVM bootup waiting for user experience optimization.
    Disable the service sleep-inhibitor start in mwc service.
    
    Signed-off-by: Wan Shuang <shuang.wan@intel.com>

--- host/src/pack/dpkg/opt/cfc/mwc/bin/loadapp.sh
+++ host/src/pack/dpkg/opt/cfc/mwc/bin/loadapp.sh
@@ -8,6 +8,46 @@ then
     exit 1
 fi
 
+# Get the number of running Android guest instances.
+num_civ_insts=`ps aux | grep qemu.*android | grep -v grep | wc -l`
+
+# Start the civ service if Android guest is not started yet.
+if [ "$num_civ_insts" -lt "1" ];
+then
+    systemctl --user start civ
+fi
+
+/opt/cfc/mwc/bin/resume_civ.sh
+
+num_adb_devices=`adb devices | grep vsock.*device | grep -v grep | wc -l`
+if [[ "$num_adb_devices" -lt "1" ]];
+then
+    adb connect "vsock:3:5555"
+fi
+
+function wait_bootcomplete() {
+  for i in {1..15}; do
+    boot_complete=`adb -s vsock:3:5555 shell getprop dev.bootcomplete`
+    if [[ "$boot_complete" -ge "1" ]];
+    then
+      echo "adb is $boot_complete when $i"
+      cuptime=`adb shell cat /proc/uptime | cut -d "." -f 1`
+      if [[ "$cuptime" -lt "15" ]];
+      then
+        echo "adb is $cuptime when $i"
+        sleep 1
+      else
+        adb -s vsock:3:5555 shell cat /proc/uptime
+        break
+      fi
+    else
+      sleep 1
+    fi
+  done
+}
+
+wait_bootcomplete
+
 adb -s vsock:3:5555 shell am start -n $2 --display $3
 
 if [[ $"NUM_PARAMS" -ge "4" ]];
